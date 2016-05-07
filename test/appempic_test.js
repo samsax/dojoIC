4a4f0a12df9aeb5c27e0ae29f63ed0f02179c6b9
System User <samuelsaxofon@gmail.com>
2016-05-07 11:38:20 -0500

    Pasar pruebas

--- test/appempic_test.js
+++ test/appempic_test.js
@@ -6,10 +6,10 @@ var assert = require('should');
 describe('appempic', function () {
 
   it('should be name', function () {
-    appempic.getName().should.equal('samuel');
+    appempic.getName().should.equal('Samuel');
   });
   it('should be lasName', function () {
-    appempic.getLastName().should.equal('arenas');
+    appempic.getLastName().should.equal('Arenas');
   });
 
 });
